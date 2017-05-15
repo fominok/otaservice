0c3f34159fe3e2d8469dec175960622d1a25f265
Evgeny Fomin <fominok@hotmail.com>
2017-05-15 14:09:16 +0300

    add independent registration tests

--- test/otaservice/api_test.clj
+++ test/otaservice/api_test.clj
@@ -11,8 +11,35 @@
 (with-state-changes [(before :facts (migrate))
                      (after :facts (rollback-all))]
   (facts "about authentication"
-         (let [register-req #(-> (mock/request :post "/api/v1/register")
-                                 (mock/content-type "application/json")
-                                 (mock/body (cheshire/generate-string %)))]
+         (let [register-req #(rest-api (-> (mock/request :post "/api/v1/register")
+                                           (mock/content-type "application/json")
+                                           (mock/body (cheshire/generate-string %))))]
+
            (fact "if register with short login/pass proper error returned"
-                 (:status (rest-api (register-req {:username "tes" :password "pass"}))) => 400))))
+                 (let [response (register-req {:username "tes" :password "passwor"})
+                       body (parse-body (:body response))]
+                   (:status response) => 400
+                   (:error body) => {:username ["Username length must be greater than 3"]
+                                     :password ["Password length must be greater than 7"]}))
+
+           (fact "correctly filled field do not show up in error message"
+                 (let [response (register-req {:username "user" :password "wrong"})
+                       body (parse-body (:body response))]
+                   (:status response) => 400
+                   (:error body) =not=> (contains {:username anything})
+                   (:error body) => (contains {:password anything})))
+
+           (fact "successful registration returns username"
+                 (let [sameuser "user"
+                       response (register-req {:username sameuser :password "password"})
+                       body (parse-body (:body response))]
+                   (:status response) => 200
+                   (:identity body) => sameuser))
+
+           (fact "registering with existing username returns proper code and error"
+                 (let [sameuser "user"
+                       _ (register-req {:username sameuser :password "password"})
+                       response (register-req {:username sameuser :password "anything"})
+                       body (parse-body (:body response))]
+                   (:status response) => 409
+                   (:error body) => "User already exists")))))
